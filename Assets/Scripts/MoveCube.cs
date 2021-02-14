d8d5f669c487a940a6fdb9f7d37dadfb9c452dc4
29244 <29244@ma-web.nl>
2021-02-14 14:22:38 +0100

    4

---
+++ Assets/Scripts/MoveCube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    public Transform StartMove;
+    public Transform EndMove;
+
+    private float startTime;
+    private float speed = 2.5f;
+    private float journeyLength;
+    private float testSpin;
+    private void Start()
+    {
+        this.transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            startTime = Time.time;
+            StartCoroutine("MoveofCube");
+        }
+    }
+
+    IEnumerator MoveofCube()
+    {
+        journeyLength = Vector3.Distance(StartMove.position, EndMove.position);
+        for (float i = StartMove.position.x; i <= 1f; i = testSpin)
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            testSpin = distCovered / journeyLength;
+            this.transform.position = Vector3.Lerp(StartMove.position, EndMove.position, testSpin);
+            yield return 0;
+        }
+    }
+}
