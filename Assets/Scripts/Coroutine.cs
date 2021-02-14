fcf83793238dabc35ca98141826187b6d5761648
29244 <29244@ma-web.nl>
2021-02-14 13:57:19 +0100

    1

---
+++ Assets/Scripts/Coroutine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutine : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("Print");
+        }
+    }
+
+    IEnumerator Print()
+    {
+        print("Ik start nu de coroutine");
+
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 1; i <= 1; i++)
+        {
+            print("Coroutine Update" + " " + i);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        print("Coroutine Einde");
+    }
+}
